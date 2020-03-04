2af6040dec8cf3a217414fade3f34f083497bd5e
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-03-04 08:24:32 +0100

    End points rearrangement

--- README.md
+++ README.md
@@ -1,29 +1,28 @@
 # COVID19Feed2Json
-Python API to returning Civil Protection Department and Italian Ministry of Health official COVID19 data in Italy. It is based on web scraping activity.
+Python API returning Italian Civil Protection Department and Ministry of Health official COVID19 data in Italy. It is based on daily web scraping activity.
 
 ## API END POINTS
 
-#### https://covid19-it-api.herokuapp.com/data
-Returns **GeoJSON** data from live web scraping of the <a target="_blank" href="http://www.protezionecivile.gov.it/web/guest/dettaglio/-/journal/rss/351561?doAsGroupId=20182&refererPlid=42041&controlPanelCategory=current_site.content&_15_groupId=20182">DPC RSS</a> source. 
-This source in particular is no more updated 
+#### https://covid19-it-api.herokuapp.com/info
+Returns **JSON** info about the project
 
 #### https://covid19-it-api.herokuapp.com/summary
-Returns **JSON** daily data about total infected people, positive, deads, recovered.
+Returns **JSON** daily data about total infected people, positive, deads, recovered.<br/>
 Optional parameter:
 - data (*type: String*, *example: 2020-02-25 18:00*)
 
 #### https://covid19-it-api.herokuapp.com/state
-Returns **JSON** daily data about the sanitary state of infected people
+Returns **JSON** daily data about the sanitary state of infected people.<br/>
 Optional parameter:
 - data (*type: String*, *example: 2020-02-25 18:00*)
 
 #### https://covid19-it-api.herokuapp.com/distribution/regions
-Returns **GeoJSON** daily data about the outbreaks in each Region
+Returns **GeoJSON** daily data about the outbreaks in each Region.<br/>
 Optional parameter:
 - data (*type: String*, *example: 2020-02-25 18:00*)
 
 #### https://covid19-it-api.herokuapp.com/distribution/regions/last
-Returns **GeoJSON** data about the last updated outbreaks in each Region 
+Returns **GeoJSON** data about the last updated outbreaks in each Region.<br/> 
 Optional parameter:
 - data (*type: String*, *example: 2020-02-25 18:00*)
 
--- api.py
+++ api.py
@@ -2,7 +2,6 @@ import feedparser
 import requests
 from lxml import html
 import json
-from parseDescription import parseAccertati
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
@@ -12,48 +11,31 @@ script_path = os.path.dirname(sys.argv[0])
 app = Flask(__name__)
 CORS(app)
 
-# SUMMARY DATA
+# API END POINTS
+
+# #####################################################################
+# PROJECT INFO
+# #####################################################################
 @app.route('/')
+@app.route('/info')
 def retrieve_app_info():
     return jsonify({
-        'Author':'Alessio Di Lorenzo','Data Source': 'Ministero della Salute'
+        'author':'Alessio Di Lorenzo',
+        'description':'Python API returning Italian Civil Protection Department and Ministry of Health official COVID19 data in Italy. It is based on daily web scraping activity',
+        'data_source': 'Ministero della Salute',
+        'project_readme':'https://github.com/alessiodl/COVID19Feed2Json/blob/master/README.md',
+        'project_home':'https://github.com/alessiodl/COVID19Feed2Json',
     })
 
+# #####################################################################
+# OUT OF DATE
+# #####################################################################
 @app.route('/data')
 def retrieve_data():
-    # RSS Protezione Civile
-    url = 'http://www.protezionecivile.gov.it/web/guest/dettaglio/-/journal/rss/351561?doAsGroupId=20182&refererPlid=42041&controlPanelCategory=current_site.content&_15_groupId=20182'
-    feed = feedparser.parse(url)
-    # News con i dettagli sui casi accertati
-    data = []
-    titles = ['i casi accertati', 'i casi', 'i contagiati', 'i positivi']
-    for post in feed.entries:
-        #if "casi accertati" in post.title or "i contagiati" in post.title:
-        if any(x in post.title for x in titles):
-            # Estrazione dati dal link nel feed
-            resp = requests.get(post.link)
-            tree = html.fromstring(resp.content)
-            subs = 'Nel dettaglio:'
-            main = tree.xpath("//div[@class='top-content-body']//text()")
-            info = [i for i in main]
-            if len(info) == 0:
-                main = tree.xpath("//div[@class='top-content-body']//p/text()")
-                info = [i for i in main]
-
-            details = [d for d in info if subs in d]
-            desc = details[0].split('Nel dettaglio:')[1].strip()
-            # Organizzazione dei dati del bollettino in un dizionario
-            dict = {
-                'titolo': post.title,
-                'aggiornamento_del': post.published,
-                'link': post.link,
-                'casi_accertati': parseAccertati(desc)
-            }
-            data.append(dict)
-    # JSON output
-    return jsonify(data)
-
-# API END POINTS
+    return jsonify({
+        'message': 'This end point is no more in use. Please, take a look to the README page in the official GitHub repository',
+        'github_readme':'https://github.com/alessiodl/COVID19Feed2Json/blob/master/README.md'
+    })
 
 # #####################################################################
 # SUMMARY DATA
